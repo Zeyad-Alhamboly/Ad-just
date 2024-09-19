87edaadbf9a5dd4994cbeabc92d3b360edd9a042
Zeyad Alhamboly <zeyadalhamboly.dev@gmail.com>
2024-09-19 11:01:46 +0300

    commit

--- src/Header/Navbar/Navbar.jsx
+++ src/Header/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
   // State to manage the navbar's visibility
@@ -12,24 +12,24 @@ const Navbar = () => {
 
   // Array containing navigation items
   const navItems = [
-    { id: 1, text: 'Home' },
-    { id: 2, text: 'Company' },
-    { id: 3, text: 'Resources' },
-    { id: 4, text: 'About' },
-    { id: 5, text: 'Contact' },
+    { id: 1, text: "Home" },
+    { id: 2, text: "Company" },
+    { id: 3, text: "Resources" },
+    { id: 4, text: "About" },
+    { id: 5, text: "Contact" },
   ];
 
   return (
-    <div className='bg-black flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+    <div className=" flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
       {/* Logo */}
-      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+      <img src="https://zeyad-alhamboly.github.io/Ad-just/logo.png" alt="" />
 
       {/* Desktop Navigation */}
-      <ul className='hidden md:flex'>
-        {navItems.map(item => (
+      <ul className="hidden md:flex">
+        {navItems.map((item) => (
           <li
             key={item.id}
-            className='p-4 hover:bg-[#00df9a] rounded-xl m-2 cursor-pointer duration-300 hover:text-black'
+            className="p-4 hover:bg-green-700 hover:text-white rounded-xl m-2 cursor-pointer duration-300 "
           >
             {item.text}
           </li>
@@ -37,7 +37,7 @@ const Navbar = () => {
       </ul>
 
       {/* Mobile Navigation Icon */}
-      <div onClick={handleNav} className='block md:hidden'>
+      <div onClick={handleNav} className="block md:hidden">
         {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
 
@@ -45,18 +45,18 @@ const Navbar = () => {
       <ul
         className={
           nav
-            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
-            : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+            ? "fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+            : "ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]"
         }
       >
         {/* Mobile Logo */}
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+        <img src="https://zeyad-alhamboly.github.io/Ad-just/logo.png" alt="" />
 
         {/* Mobile Navigation Items */}
-        {navItems.map(item => (
+        {navItems.map((item) => (
           <li
             key={item.id}
-            className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
+            className="p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600"
           >
             {item.text}
           </li>
